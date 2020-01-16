56eae32ff6a7c38569a338436a136b3d6acb2ba1
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2020-01-16 11:38:31 +0530

    APIs are ready

--- common/authUtils.js
+++ common/authUtils.js
@@ -1,5 +1,27 @@
 import { getClientDetails } from '../services/ClientService';
 
+import uuid from 'node-uuid';
+const newSessionRoutes = [{ path: '/user/login', method: 'POST' }];
+const authRoutes = [{ path: '/user/password', method: 'PUT' }];
+
+export const isNewSessionRequired = (httpMethod, url) => {
+    for (let routeObj of newSessionRoutes) {
+      if (routeObj.method === httpMethod && routeObj.path === url) {
+        return true;
+      }
+    }
+    return false;
+}
+
+export const isAuthRequired = (httpMethod, url) => {
+    for (let routeObj of authRoutes) {
+        if (routeObj.method === httpMethod && routeObj.path === url) {
+        return true;
+        }
+    }
+    return false;
+}
+
 export const clientApiKeyValidation = async (req, res, next) => {
 
     let clientApiKey = req.get('api_key');
@@ -23,5 +45,19 @@ export const clientApiKeyValidation = async (req, res, next) => {
             response: "Invalid Api Key"
         })
     }
+}
 
+export const generateRandomSessionID = () => {
+    return uuid.v4();
+}
+
+export const getRedisSessionData = (redisClient,sessionId) => {
+    return new Promise((resolve, reject) => {
+      redisClient.get(sessionId, function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    })
 }
